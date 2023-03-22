d3e0c8c0e03bb0a7739886f7d5f7584b19ec6812
Thomas <dinhcongthong97y@gmail.com>
2023-03-22 21:28:31 +0700

    make conflic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Mail;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
+     * Create a new controller instance. etsttsfdsf
      *
      * @return void
      */
